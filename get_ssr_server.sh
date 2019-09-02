d283ed18f26d82bd789c2239bb84fbc4b2039581
241-centos <241@qiujieda.com>
2019-09-02 12:49:41 +0800

    update

--- get_ssr_server.sh
+++ get_ssr_server.sh
@@ -21,9 +21,8 @@ echo "" > ssr.txt
 curl $URL -A "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36" | grep 'link="ssr://' | while read line; do OPT "$line"; done
 echo "" >> ssr.txt
 
-SER=`echo -n `cat ssr.txt` | base64`
-SER=`echo -n $SER | sed s/[[:space:]]//g`
-echo $SER
+echo -n `cat ssr.txt` | base64 | sed s/[[:space:]]//g > ssr_base64.txt
+cat ssr_base64.txt
 
 echo "=> "`date`". End sync ssr server"
 
