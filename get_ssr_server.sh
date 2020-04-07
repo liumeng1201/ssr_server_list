e26040892d9dac1741bc893b323734b53824fdfd
241-centos <241@qiujieda.com>
2020-04-07 12:05:54 +0800

    update

--- get_ssr_server.sh
+++ get_ssr_server.sh
@@ -2,30 +2,16 @@
 
 WORK_DIR="/home/lm/ssr"
 SSR_FILE=`echo $WORK_DIR"/ssr.txt"`
-TMP=`echo $WORK_DIR"/tmp"`
-SSR_BASE64_FILE=`echo $WORK_DIR"/ssr_base64.txt"`
 
-URL="https://raw.githubusercontent.com/nulastudio/Freedom/master/docs/index.html"
+URL="https://www.liesauer.net/yogurt/subscribe?ACCESS_TOKEN=d309c6921bbd7d0d"
 
 echo "=> Change work dir."
 cd ${WORK_DIR}
 
-OPT()
-{
-    line=$1
-    line=${line##*'link="'}
-    line=${line%%'">'*}
-    echo $line >> ${SSR_FILE}
-}
-
 echo "=> "`date`". Start sync ssr server."
 
 echo "" > ${SSR_FILE}
-curl $URL -A "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36" | grep 'link="ssr://' | while read line; do OPT "$line"; done
-echo "" >> ${SSR_FILE}
-
-echo -n `cat ${SSR_FILE}` | base64 > ${TMP}
-echo -n `cat ${TMP}` | sed s/[[:space:]]//g > ${SSR_BASE64_FILE}
+curl $URL -A "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36" | base64 -d > ${SSR_FILE}
 
 echo "=> "`date`". End sync ssr server"
 
